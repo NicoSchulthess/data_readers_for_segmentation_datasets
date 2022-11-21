f3e598041167155750fbacfc6a1896c9fe027201
Nico Schulthess <scnico@ethz.ch>
2022-11-21 15:19:35 +0100

    Can load and save multiple datasets in one run

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import load_datasets
 # ================================
 # 'brain' / 'cardiac' / 'prostate'
 # ================================
-ANATOMY = 'prostate'
+ANATOMIES = ['brain', 'brain', 'brain', 'brain']
 
 # ================================
 # set dataset here.
@@ -17,18 +17,16 @@ ANATOMY = 'prostate'
 # for cardiac: 'ACDC' / 'RVSC'
 # for prostate: 'NCI' / 'PIRAD_ERC'
 # ================================
-DATASET = 'NCI' 
-
-# ================================
-# set train / test / validation here
-# ================================
-# 'train' / 'test' / 'validation'
-TRAIN_TEST_VALIDATION = 'train'
+DATASETS = ['HCP_T1', 'HCP_T2', 'ABIDE_caltech', 'ABIDE_stanford']
 
 # ================================
 # read images and segmentation labels
 # ================================
-images, labels = load_datasets.load_dataset(anatomy = ANATOMY,
-                                            dataset = DATASET,
-                                            train_test_validation = TRAIN_TEST_VALIDATION,
-                                            first_run = False)  # <-- SET TO TRUE FOR THE FIRST RUN (enables preliminary preprocessing e.g. bias field correction)
+for data_split in ['train', 'test', 'validation']:
+    for dataset, anatomy in zip(DATASETS, ANATOMIES):
+        images, labels = load_datasets.load_dataset(
+            anatomy=anatomy,
+            dataset=dataset,
+            train_test_validation=data_split,
+            first_run = False  # <-- SET TO TRUE FOR THE FIRST RUN (enables preliminary preprocessing e.g. bias field correction)
+)
